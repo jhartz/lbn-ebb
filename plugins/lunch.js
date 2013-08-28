dbd8e7ff9a40c5a9bba898475c11044920eaec5f
Jake Hartz <jhartz@outlook.com>
2013-08-28 10:47:35 -0400

    Fixed to match new HS lunch

--- plugins/lunch.js
+++ plugins/lunch.js
@@ -1,19 +1,13 @@
 ebb.plugins.push({
     name: "Lunch",
     content: [
-        '<div class="title">Main Line</div>$$%LBNEBB_PLUGIN_VAR1@@#',
-        '<div class="title">Express Line</div>$$%LBNEBB_PLUGIN_VAR2@@#'
+        '<div class="title">Lunch</div>$$%LBNEBB_PLUGIN_VAR1@@#'
     ],
     options: [
         {
-            name: "Main Line",
+            name: "Lunch",
             type: "filteredtextarea",
             variable: "$$%LBNEBB_PLUGIN_VAR1@@#"
-        },
-        {
-            name: "Express Line",
-            type: "filteredtextarea",
-            variable: "$$%LBNEBB_PLUGIN_VAR2@@#"
         }
     ]
 });
