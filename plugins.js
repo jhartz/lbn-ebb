d8f825c2a03067243eb4d7abd7267f33c00dd1b1
Jake Hartz <jhartz@outlook.com>
2013-11-20 23:00:15 -0500

    Added example of plugin disabled in plugins.js

--- plugins.js
+++ plugins.js
@@ -1,1 +1,10 @@
-var plugin_list = ["title.js", "datetime.js", "cycle-day.js", "bctc.js", "lunch.js", "main-announcements.js", "sports.js", "weather.js"];
+var plugin_list = [
+    "title.js",
+    "datetime.js",
+    "cycle-day.js",
+    //"bctc.js",
+    "lunch.js",
+    "main-announcements.js",
+    "sports.js",
+    "weather.js"
+];
